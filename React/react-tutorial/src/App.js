90ee2216dfee81bb1394fcfc3e7be64a1b8688ce
Evan <evanmlongwood@gmail.com>
2019-04-19 15:34:07 -0700

    Added Tables.

--- React/react-tutorial/src/App.js
+++ React/react-tutorial/src/App.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import Table from './table.js'
+
 
 class App extends Component {
     render() {
-        return (
-            <div className="App">
-                <h1>Hello, React!</h1>
-            </div>
-        )
+        return ( < div
+        className = "container" >
+            < Table / >
+            < /div>
+    )
+        ;
     }
 }
 
